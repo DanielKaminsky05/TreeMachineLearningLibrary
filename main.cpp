f38675b2613d5c30a901b63bc6c09864380acf8f
Thomson-Lam <thomson.lam327@gmail.com>
2025-11-06 15:11:20 -0500

    checkpoint: main.cpp setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "code/MLSuite/Dataset.h"
+#include <vector>
 
 int main() {
-	std::cout << "Hello Assignment 2!" << std::endl;
-    return 0;
+    try {
+        Dataset iris_train("data-preprocessing/data-files/classification/iris_dataset/iris_X_train_processed.csv", "train");
+        std::vector<float> data = iris_train.get_data();
+        for(int i = 0; i < 10; ++i) {
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0; 
 }

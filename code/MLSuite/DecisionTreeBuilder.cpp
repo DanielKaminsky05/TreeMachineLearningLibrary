a356bb6b6f9a82a00926b487eb1501872bc3abd2
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2025-12-10 15:31:45 -0500

    added validation

--- code/MLSuite/DecisionTreeBuilder.cpp
+++ code/MLSuite/DecisionTreeBuilder.cpp
@@ -1,14 +1,21 @@
 #include "DecisionTreeBuilder.h"
+#include <stdexcept> // For std::invalid_argument
 
 DecisionTreeBuilder::DecisionTreeBuilder() 
     : mMaxDepth(10), mMinSamplesSplit(2) {}
 
 DecisionTreeBuilder& DecisionTreeBuilder::setMaxDepth(int maxDepth) {
+    if (maxDepth <= 0) {
+        throw std::invalid_argument("maxDepth must be a positive integer.");
+    }
     mMaxDepth = maxDepth;
     return *this;
 }
 
 DecisionTreeBuilder& DecisionTreeBuilder::setMinSamplesSplit(int minSamplesSplit) {
+    if (minSamplesSplit < 2) {
+        throw std::invalid_argument("minSamplesSplit must be at least 2.");
+    }
     mMinSamplesSplit = minSamplesSplit;
     return *this;
 }

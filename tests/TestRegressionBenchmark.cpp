929642ee12d477cbd385802c94f838d966426748
DanielKaminsky05 <daniel.kaminsky23@gmail.com>
2025-12-19 15:36:08 -0500

    added more test cases

--- tests/TestRegressionBenchmark.cpp
+++ tests/TestRegressionBenchmark.cpp
@@ -3,48 +3,92 @@
 #include "MockModel.h"
 #include "../code/MLSuite/RegressionBenchmark.h"
 #include "../code/MLSuite/Dataset.h"
+#include <fstream>
+#include <cstdio>
 
 using ::testing:: _;
 using ::testing::Return;
 using ::testing::AtLeast;
 
-// Interaction Test using Mock Object
 class RegressionBenchmarkTest : public ::testing::Test {
 protected:
+    std::string dummyFile = "dummy.csv";
+
     void SetUp() override {
-        // Setup code if needed
+        // Create a simple dummy CSV for Dataset loading
+        std::ofstream ofs(dummyFile);
+        ofs << "col1\n1.0\n2.0";
+        ofs.close();
+    }
+
+    void TearDown() override {
+        std::remove(dummyFile.c_str());
     }
 };
 
-TEST_F(RegressionBenchmarkTest, ExecuteCallsFitAndPredict) {
+TEST_F(RegressionBenchmarkTest, HappyPath_CallsPredictAndGetName) {
     MockModel mockModel;
     RegressionBenchmark benchmark;
 
-    // RegressionBenchmark::execute calls model.predict but not model.fit.
-    // The Dataset objects are configured to have 2 data points for simple testing.
-    
-    std::string dummyFile = "dummy.csv";
-    std::ofstream ofs(dummyFile);
-    ofs << "target\n1.0\n2.0";
-    ofs.close();
-
-    Dataset xData(dummyFile, "train"); // type doesn't matter much for this test
+    Dataset xData(dummyFile, "train");
     Dataset yData(dummyFile, "train");
 
+    // Verify loaded data size
+    ASSERT_EQ(xData.get_data().size(), 2) << "Dataset failed to load expected 2 elements";
+
+    // Expect predict to be called once with correct arguments logic (implicit via matcher)
+    // and return a vector matching yData size (2 elements)
     EXPECT_CALL(mockModel, predict(_, _))
-        .Times(::testing::AtLeast(1))
-        .WillRepeatedly(Return(std::vector<float>{1.0f, 2.0f})); // Return dummy predictions matching size
+        .Times(1)
+        .WillOnce(Return(std::vector<float>{1.0f, 2.0f}));
 
+    // Expect getName to be called for printing results
     EXPECT_CALL(mockModel, getName())
+        .Times(AtLeast(1))
         .WillRepeatedly(Return("MockModel"));
 
     benchmark.execute(mockModel, xData, yData);
+}
 
+TEST_F(RegressionBenchmarkTest, ErrorCase_MismatchedSize_DoesNotPrintResults) {
+    MockModel mockModel;
+    RegressionBenchmark benchmark;
+
+    Dataset xData(dummyFile, "train");
+    Dataset yData(dummyFile, "train");
+    ASSERT_EQ(xData.get_data().size(), 2);
+
+    // Return a vector of size 3 (yData has size 2)
+    EXPECT_CALL(mockModel, predict(_, _))
+        .Times(1)
+        .WillOnce(Return(std::vector<float>{1.0f, 2.0f, 3.0f}));
+
+    // getName is called at the beginning of execute to populate result struct.
     EXPECT_CALL(mockModel, getName())
+        .Times(AtLeast(1))
         .WillRepeatedly(Return("MockModel"));
 
     benchmark.execute(mockModel, xData, yData);
-    
-    // Clean up
-    std::remove(dummyFile.c_str());
+}
+
+TEST_F(RegressionBenchmarkTest, ErrorCase_EmptyPrediction) {
+    MockModel mockModel;
+    RegressionBenchmark benchmark;
+
+    Dataset xData(dummyFile, "train");
+    Dataset yData(dummyFile, "train");
+    ASSERT_EQ(xData.get_data().size(), 2);
+
+    // Return empty predictions
+    EXPECT_CALL(mockModel, predict(_, _))
+        .Times(1)
+        .WillOnce(Return(std::vector<float>{}));
+
+    // Should return early due to mismatch (0 != 2)
+    // But getName is called early
+    EXPECT_CALL(mockModel, getName())
+        .Times(AtLeast(1))
+        .WillRepeatedly(Return("MockModel"));
+
+    benchmark.execute(mockModel, xData, yData);
 }
